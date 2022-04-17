44a24b2124df49cb4814b5d91d54f5da2d3f874c
Imran Hossen Bappy <61227100+imranBappy@users.noreply.github.com>
2022-04-18 02:24:24 +0600

    bloge page include layout

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Layout from '../components/Layout';
 
 const blogs = () => {
     return (
-        <div>
+        <Layout>
             <h1>Blogs</h1>
-        </div>
+        </Layout>
     );
 };
 
